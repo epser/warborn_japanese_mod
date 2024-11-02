7dfbef8ad472278c9893f03156b90ace131fcc3e
epser <eps_r@eps-r.net>
2024-11-02 16:56:49 +0900

    Modモードと言語の切り替え たとえばローカライズ文字列を保管しておく

--- plugin/LocalizationManagerPatch.cs
+++ plugin/LocalizationManagerPatch.cs
@@ -15,12 +15,25 @@ using TMPro;
 
 namespace JapaneseMod
 {
+
+    [HarmonyPatch(typeof(LocalizationManager), MethodType.Constructor)]
+    public static class LocalizationManagerConstructorPatch
+    {
+        public static void Postfix(LocalizationManager __instance)
+        {
+            Plugin.Logger.LogInfo("LocalizationManager constructor is called!");
+            Plugin.LanguageManagerReference = __instance;
+        }
+    }
+
+
     [HarmonyPatch(typeof(LocalizationManager), "LoadStringsForLocale")]
     public static class LoadStringsForLocalePatch
     {
         // public void LoadStringsForLocale(string locale, bool silent = false)
         public static bool Prefix(LocalizationManager __instance, ref Dictionary<string, string> ___LocalizedStrings, ref LocalizationManager.LanguageChangedHandler ___LanguageChanged, object[] __args)
         {
+            Plugin.Logger.LogInfo("LoadStringsForLocale is called!");
             string text = (string)__args[0];
             bool silent = (bool)__args[1];
             if (!LocalizationManager.AvailableLanguageKeys.Contains(text))
@@ -48,7 +61,34 @@ namespace JapaneseMod
                     return false; // 元の関数を実行しない
                 }
             }
+            // 上書き日本語の存在とは関係なく、en-GBとja-JPはPlugin.StoredLanguageStringsに保存しておく
+            Plugin.StoredLanguageStrings["en-GB"] = LoadJson(
+                BaseGame.Content.GetLoadedAsset<TextAsset>(LocalizationManager.LanguageFiles["en-GB"] + ".json")
+            );
+            Plugin.StoredLanguageStrings["ja-JP"] = LoadJson(
+                BaseGame.Content.GetLoadedAsset<TextAsset>(LocalizationManager.LanguageFiles["ja-JP"] + ".json")
+            );
+
             return true; // 元の関数を実行
         }
+
+        // jsonを辞書に展開
+        public static Dictionary<string, string> LoadJson(TextAsset json)
+        {
+            if (json != null)
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(json.text);
+            }
+            return null;
+        }
     }
+
+    ///**
+    // * GetLocalizedStringのパッチ
+    // */
+    //[HarmonyPatch(typeof(LocalizationManager), "GetLocalizedString")]
+    //public static class GetLocalizedStringPatch
+    //{
+    //}
+
 }
--- plugin/Plugin.cs
+++ plugin/Plugin.cs
@@ -2,9 +2,12 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using JapaneseMod.@struct;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Warborn;
 
 namespace JapaneseMod;
 
@@ -12,13 +15,23 @@ namespace JapaneseMod;
 [BepInProcess("WARBORN.exe")]
 public class Plugin : BaseUnityPlugin
 {
+    // static
     internal static new ManualLogSource Logger;
     internal static ModConfig ConfigStruct;
     internal static AssetOverwrite Assets;
-    internal static IInputSystem Input;
+    internal static Dictionary<string, Dictionary<string, string>> StoredLanguageStrings;
+    internal static Dictionary<string, string> StoredLocalizedStrings;
+    internal static bool IsPatchEnabled = true;
 
+    // private
+    private IInputSystem Input;
     private KeyCode MainKeyForLanguageSwitch;
     private IEnumerable<KeyCode> ModifierKeysForLanguageSwitch;
+    private KeyCode MainKeyForPatchSwitch;
+    private IEnumerable<KeyCode> ModifierKeysForPatchSwitch;
+
+    // original classes
+    internal static LocalizationManager LanguageManagerReference;
 
     private void Awake()
     {
@@ -31,31 +44,68 @@ public class Plugin : BaseUnityPlugin
 
         // Load config
         ConfigStruct = new ModConfig(Config);
-        MainKeyForLanguageSwitch = ConfigStruct.languageSwitchKey.Value.MainKey;
-        ModifierKeysForLanguageSwitch = ConfigStruct.languageSwitchKey.Value.Modifiers;
 
         // フォントの読み込み
         Assets = new AssetOverwrite(ConfigStruct);
 
         // Input system
         Input = UnityInput.Current;
+
+        StoredLanguageStrings = new Dictionary<string, Dictionary<string, string>>();
+        StoredLocalizedStrings = new Dictionary<string, string>();
     }
 
+    /**
+     * 毎フレーム呼ばれる処理
+     */
     private void Update()
     {
-        // Plugin update logic
-        IsPressedKeys(MainKeyForLanguageSwitch, ModifierKeysForLanguageSwitch);
+        if(IsPressedKeys(ConfigStruct.languageSwitchKey.Value.MainKey, ConfigStruct.languageSwitchKey.Value.Modifiers))
+        {
+            SwitchLanguage();
+        }
+
+        if(IsPressedKeys(ConfigStruct.modSwitchKey.Value.MainKey, ConfigStruct.modSwitchKey.Value.Modifiers))
+        {
+            SwitchModMode();
+        }
     }
 
-    private void IsPressedKeys(KeyCode mainKey, IEnumerable<KeyCode> modifiers)
+    private bool IsPressedKeys(KeyCode mainKey, IEnumerable<KeyCode> modifiers)
     {
-        if (modifiers.All(Input.GetKey))
+        if (Input.GetKeyDown(mainKey) && modifiers.All(Input.GetKey))
         {
-            if (Input.GetKeyDown(mainKey))
-            {
-                Logger.LogInfo("Language switch key is pressed!");
-                // ここに言語切り替えの処理を書く
-            }
+            Input.GetKeyDown(mainKey);
+            return true;
+        }
+        return false;
+    }
+
+    private void SwitchModMode()
+    {
+        IsPatchEnabled = !IsPatchEnabled;
+        LanguageManagerReference.LoadStringsForLocale(LanguageManagerReference.CurrentLanguageKey, false);
+        Logger.LogInfo($"Patch is {(IsPatchEnabled ? "enabled" : "disabled")}");
+    }
+
+    private void SwitchLanguage()
+    {
+        if (LanguageManagerReference.CurrentLanguageKey == null)
+        {
+            Logger.LogInfo("CurrentLanguageKey is null");
+            return;
+        }
+
+        // ja-JP と en-GB の間をトグル
+        if (LanguageManagerReference.CurrentLanguageKey == "ja-JP")
+        {
+            Logger.LogInfo("Switching to en-GB");
+            LanguageManagerReference.LoadStringsForLocale("en-GB", false);
+        }
+        else
+        {
+            Logger.LogInfo("Switching to ja-JP");
+            LanguageManagerReference.LoadStringsForLocale("ja-JP", false);
         }
     }
 }
