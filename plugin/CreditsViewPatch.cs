6d86f104ec7041d654cbd0825846fdd666c4535c
epser <eps_r@eps-r.net>
2024-11-17 06:08:23 +0900

    クレジット調整 字幕サイズと、ワクの引く際のアンカーポイント

--- plugin/CreditsViewPatch.cs
+++ plugin/CreditsViewPatch.cs
@@ -48,22 +48,24 @@ namespace JapaneseMod
         public static void Postfix(ref CreditsView __instance)
         {
             CreditsViewPatchVariables.TextBg = __instance.AddNewChildImageView("Logo", CreateSolidColorSprite(new Color(0f, 0f, 0f, 0.9f)));
-            CreditsViewPatchVariables.EnglishLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.BootAssetsReference.AlternateFont, 44, Color.white, TextAlignmentOptions.Center);
-            CreditsViewPatchVariables.CommonLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.BootAssetsReference.AlternateFont, 44, Color.white, TextAlignmentOptions.Center);
-            CreditsViewPatchVariables.JapaneseLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.Assets.StoredLanguageFonts.Where(font => font.LanguageCode == Plugin.LANGUAGE_JA_JP && font.Type == FontType.AlternateOutline && font.PatchMode).FirstOrDefault().Font, 44, Color.white, TextAlignmentOptions.Center);
+            CreditsViewPatchVariables.EnglishLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.BootAssetsReference.AlternateFont, 32, Color.white, TextAlignmentOptions.Center);
+            CreditsViewPatchVariables.CommonLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.BootAssetsReference.AlternateFont, 32, Color.white, TextAlignmentOptions.Center);
+            CreditsViewPatchVariables.JapaneseLyrics = __instance.AddNewChildTextView("Lyrics", "", Plugin.Assets.StoredLanguageFonts.Where(font => font.LanguageCode == Plugin.LANGUAGE_JA_JP && font.Type == FontType.AlternateOutline && font.PatchMode).FirstOrDefault().Font, 32, Color.white, TextAlignmentOptions.Center);
 
+            CreditsViewPatchVariables.TextBg.SetSize(1920f, 1080 / 100 * 12);
             CreditsViewPatchVariables.TextBg.SetBottomPosition(0);
-            CreditsViewPatchVariables.TextBg.SetSize(1920f, 1080 / 100 * 18);
+            CreditsViewPatchVariables.TextBg.RectTransform.anchorMin = new Vector2(0, 0);
+            CreditsViewPatchVariables.TextBg.RectTransform.anchorMax = new Vector2(1, 0);
 
-            CreditsViewPatchVariables.EnglishLyrics.SetBottomPosition(1080 / 100 * 8);
+            CreditsViewPatchVariables.EnglishLyrics.SetBottomPosition(1080 / 100 * 6);
             CreditsViewPatchVariables.EnglishLyrics.SetWidth(1920f);
             Game.Common.ApplyDefaultBlueGradient(CreditsViewPatchVariables.EnglishLyrics);
 
-            CreditsViewPatchVariables.CommonLyrics.SetBottomPosition(1080 / 100 * 5);
+            CreditsViewPatchVariables.CommonLyrics.SetBottomPosition(1080 / 100 * 3.5f);
             CreditsViewPatchVariables.CommonLyrics.SetWidth(1920f);
             Game.Common.ApplyDefaultBlueGradient(CreditsViewPatchVariables.CommonLyrics);
 
-            CreditsViewPatchVariables.JapaneseLyrics.SetBottomPosition(1080 / 100 * 2);
+            CreditsViewPatchVariables.JapaneseLyrics.SetBottomPosition(1080 / 100 * 1);
             CreditsViewPatchVariables.JapaneseLyrics.SetWidth(1920f);
             Game.Common.ApplyDefaultBlueGradient(CreditsViewPatchVariables.JapaneseLyrics);
 
@@ -143,7 +145,7 @@ namespace JapaneseMod
 
                 CreditsViewPatchVariables.EnglishLyrics.ConfigureText("We're credulous");
                 CreditsViewPatchVariables.JapaneseLyrics.ConfigureText("染めてく");
-                yield return new WaitForSeconds(3.7f);
+                yield return new WaitForSeconds(3.5f);
 
                 CreditsViewPatchVariables.EnglishLyrics.ConfigureText("Walking on coals    The fire in your eyes");
                 CreditsViewPatchVariables.JapaneseLyrics.ConfigureText("灼熱の道　燃える瞳");
@@ -186,10 +188,10 @@ namespace JapaneseMod
                 CreditsViewPatchVariables.EnglishLyrics.ConfigureText("");
                 CreditsViewPatchVariables.CommonLyrics.ConfigureText("");
                 CreditsViewPatchVariables.JapaneseLyrics.ConfigureText("");
-                yield return new WaitForSeconds(4.5f);
+                yield return new WaitForSeconds(5.5f);
 
-                CreditsViewPatchVariables.TextBg.ShowOrHideViewOffscreen(false, false, 0, 4.5f, true, null);
-                yield return new WaitForSeconds(5f);
+                CreditsViewPatchVariables.TextBg.ShowOrHideViewOffscreenInDirection(RectTransform.Edge.Bottom, false, 0, 5.5f, true, null);
+                yield return new WaitForSeconds(6f);
 
                 yield break;
 
