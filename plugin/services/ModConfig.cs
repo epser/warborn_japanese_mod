e81cd26bec9048085db96af7a9b423358b4e1362
epser <eps_r@eps-r.net>
2024-11-17 19:10:25 +0900

    デフォルト設定調整

--- plugin/services/ModConfig.cs
+++ plugin/services/ModConfig.cs
@@ -29,32 +29,32 @@ namespace JapaneseMod.services
 
             jpDefaultOutlineFontName = config.Bind(
                 "AssetBundle",
-                "DefaultOutlineFontName",
+                "JPDefaultOutlineFontName",
                 "NotoSansJP-Bold Outline SDF",
                 "Heading font name(without underlay)"
             );
             jpAlternateFontName = config.Bind(
                 "AssetBundle",
-                "AlternateFontName",
-                "NotoSansJP-Light SDF",
+                "JPAlternateFontName",
+                "NotoSansJP-Regular SDF",
                 "Body font name(with underlay)"
             );
             jpAlternateOutlineFontName = config.Bind(
                 "AssetBundle",
-                "AlternateOutlineFontName",
-                "NotoSansJP-Light Outline SDF",
+                "JPAlternateOutlineFontName",
+                "NotoSansJP-Regular Outline SDF",
                 "Body font name(without underlay)"
             );
             jpActionOutlineFontName = config.Bind(
                 "AssetBundle",
-                "ActionOutlineFontName",
-                "NotoSansJP-Bold Outline SDF",
+                "JPActionOutlineFontName",
+                "NotoSansJP-Light Outline SDF",
                 "Action font name(without underlay)"
             );
 
             useEnglishDesiginingFonts = config.Bind("Design", "UseEnglishDesiginingFonts", false, "Use English designing fonts");
-            modSwitchKey = config.Bind("Key", "ModSwitchKey", new KeyboardShortcut(KeyCode.F5, [KeyCode.LeftAlt]), "Switch mod key");
-            languageSwitchKey = config.Bind("Key", "LanguageSwitchKey", new KeyboardShortcut(KeyCode.F6, [KeyCode.LeftAlt]), "Switch language key");
+            modSwitchKey = config.Bind("Key", "ModSwitchKey", new KeyboardShortcut(KeyCode.F5), "Switch mod key");
+            languageSwitchKey = config.Bind("Key", "LanguageSwitchKey", new KeyboardShortcut(KeyCode.F6), "Switch language key");
         }
     }
 }
